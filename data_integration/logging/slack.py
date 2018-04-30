1a0de3f566aeac324326ad0e7a6d089e4fe98939
Martin Loetzsch <martin.loetzsch@gmail.com>
2018-04-30 11:20:18 +0200

    Simplify message sending

--- data_integration/logging/slack.py
+++ data_integration/logging/slack.py
@@ -1,3 +1,5 @@
+"""Slack notifications for failed node runs"""
+
 import flask
 import requests
 
@@ -10,7 +12,7 @@ class Slack(events.EventHandler):
 
     def handle_event(self, event: events.Event):
         """
-        Slack event handler. Send a notification to a slack channel with respect to the current event.
+        Send the output of a node to Slack when the node failed.
         Args:
             event: The current event of interest
         """
@@ -29,11 +31,27 @@ class Slack(events.EventHandler):
         elif isinstance(event, events.NodeFinished):
             key = tuple(event.node_path)
             if not event.succeeded and event.is_pipeline is False:
-                self.send_message(
-                    message='\n:baby_chick: Ooops, a hiccup in ' +
-                            '_ <' + config.base_url() + flask.url_for('data_integration.node_page', path='/'.join(
-                        event.node_path)) + ' | ' + '/'.join(event.node_path) + ' > _',
-                    output=self.node_output[key][False], error_output=self.node_output[key][True])
+
+                message = {'text': '\n:baby_chick: Ooops, a hiccup in ' + '_ <' + config.base_url()
+                                   + flask.url_for('data_integration.node_page', path='/'.join(event.node_path))
+                                   + ' | ' + '/'.join(event.node_path) + ' > _',
+                           'attachments': []}
+
+                if (self.node_output[key][False]):
+                    message['attachments'].append({'text': self.format_output(self.node_output[key][False]),
+                                                   'mrkdwn_in': ['text']})
+
+                if (self.node_output[key][True]):
+                    message['attachments'].append({'text': self.format_output(self.node_output[key][True]),
+                                                   'color': '#eb4d5c', 'mrkdwn_in': ['text']})
+
+                response = requests.post('https://hooks.slack.com/services/' + config.slack_token(), json=message)
+
+                if response.status_code != 200:
+                    raise ValueError(
+                        'Request to slack returned an error %s. The response is:\n%s' % (
+                            response.status_code, response.text)
+                    )
 
     def format_output(self, output_events: [events.Output]):
         output, last_format = '', ''
@@ -52,32 +70,3 @@ class Slack(events.EventHandler):
 
             last_format = event.format
         return output
-
-
-    def send_message(self, message: str, output: events.Event = None, error_output: events.Event = None):
-        """
-        Sends a notification through a post request to an incoming-webhook channel.
-        Args:
-            message: The message to be sent
-            output: verbatim output as an attachment to the notification
-            error_output: error output event as an attachment to the notification
-        """
-
-        message = {'text': message, 'attachments': []}
-
-        if (output):
-            message['attachments'].append(
-                {'text': self.format_output(output),
-                 'mrkdwn_in': ['text']})
-        if (error_output):
-            message['attachments'].append(
-                {'text': self.format_output(error_output),
-                 'color': '#eb4d5c',
-                 'mrkdwn_in': ['text']})
-
-        response = requests.post('https://hooks.slack.com/services/' + config.slack_token(), json=message)
-
-        if response.status_code != 200:
-            raise ValueError(
-                'Request to slack returned an error %s. The response is:\n%s' % (response.status_code, response.text)
-            )
